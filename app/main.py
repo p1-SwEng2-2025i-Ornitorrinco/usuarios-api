b9421a3ba9fe5cec634ad282739d1e74fd5ba08a
aacelas <aacelas@unal.edu.co>
2025-07-21 18:46:49 -0500

    Cambio en el CORS

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ app = FastAPI(title="API de Usuarios")
 # 🔵 Añadir middleware de CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  
+    allow_origins=["*"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
